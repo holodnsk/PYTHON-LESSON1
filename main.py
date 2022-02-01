e2e689d0c087a0c70e53ed9fe3e64ddd0d18cc59
kholodkov <holodnsk@gmail.com>
2022-02-01 14:48:17 +0700

    print USD exchange rate

---
+++ main.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+url = 'https://www.cbr-xml-daily.ru/daily_json.js'
+
+response = requests.get(url)
+data = json.loads(response.text)
+
+print(data['Valute']['USD']['Name'])
+print(data['Valute']['USD']['Value'])
